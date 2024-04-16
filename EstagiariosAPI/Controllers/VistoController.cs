d10fb3542d5f6aeba1cc4f12a9bf770bc5341871
Vitor Moutim <moutimg@gmail.com>
2024-04-16 13:37:21 -0300

    feat: cria endpoint para adicionar e remover itens da lista de visto

---
+++ EstagiariosAPI/Controllers/VistoController.cs
@@ -0,0 +1,57 @@
+﻿using EstagiariosAPI.Database;
+using EstagiariosAPI.Entities;
+using EstagiariosAPI.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstagiariosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VistoController : ControllerBase
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public VistoController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("Adicionar/{userId}")]
+        public async Task<IActionResult> AddMovieInVistos(int userId, AddMovie body)
+        {
+            Filme movie = new Filme
+            {
+                Nome = body.Nome,
+                IdAPI = body.IdAPI,
+                BackdropPath = body.BackdropPath,
+            };
+            _dbContext.Filmes.Add(movie);
+            await _dbContext.SaveChangesAsync();
+
+            Usuario user = await _dbContext.Usuarios.FindAsync(userId);
+
+            user.Filmes.Add(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(new Message("Filme adicionado com sucesso!"));
+        }
+
+        [HttpDelete("Deletar/{userId}/{movieId}")]
+        public async Task<IActionResult> RemoveMovieInWatchList(int userId, int movieId)
+        {
+            var user = await _dbContext.Usuarios.FindAsync(userId);
+
+            Filme movieWatchlist = user.Filmes.FirstOrDefault(f => f.Id == movieId);
+            Filme movie = await _dbContext.Filmes.FindAsync(movieId);
+
+            user.Filmes.Remove(movieWatchlist);
+            _dbContext.Filmes.Remove(movie);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(new Message("Filme removido com sucesso!"));
+        }
+    }
+}
--- EstagiariosAPI/obj/Debug/net8.0/EstagiariosAPI.AssemblyInfo.cs
+++ EstagiariosAPI/obj/Debug/net8.0/EstagiariosAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstagiariosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80519e3a9d6dd422a764fbae5c5dec9a7cfeace6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19883894f2b023b45c783b021e71581baa550c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstagiariosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstagiariosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
