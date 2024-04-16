80519e3a9d6dd422a764fbae5c5dec9a7cfeace6
Vitor Moutim <moutimg@gmail.com>
2024-04-16 11:03:26 -0300

    feat: cria endpoint para realizar login

---
+++ EstagiariosAPI/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using EstagiariosAPI.Database;
+using EstagiariosAPI.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstagiariosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public LoginController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login body)
+        {
+            var user = await _dbContext.Usuarios.FirstOrDefaultAsync(u => u.Email == body.Email);
+
+            if (user == null)
+            {
+                return NotFound(new Message("Não existe usuário cadastrado com esse email."));
+            }
+
+            if (user.Senha != body.Senha)
+            {
+                return Unauthorized(new Message("Senha incorreta!"));
+            }
+
+            return Ok(new LoginSuccess
+            {
+                Id = user.Id,
+                Nome = user.Nome
+            });
+        }
+    }
+}
---
+++ EstagiariosAPI/ViewModels/LoginSuccess.cs
@@ -0,0 +1,8 @@
+﻿namespace EstagiariosAPI.ViewModels
+{
+    public class LoginSuccess
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
